0a2b88360050ffac40a11c1d6cf79168e00e4465
Niteshrana0007 <52644947+Niteshrana0007@users.noreply.github.com>
2019-07-16 18:49:48 +0530

    Update sendEmail.py

--- sendEmail.py
+++ sendEmail.py
@@ -2,6 +2,6 @@ import smtplib
 connection = smtplib.SMTP('smtp.gmail.com',587)
 connection.ehlo()
 connection.starttls()
-connection.login('niteshrana754@gmail.com','password')
-connection.sendmail('niteshrana754@gmail.com','niteshrana754@gmail.com','this is message')
+connection.login('niteshrana@gmail.com','password')
+connection.sendmail('niteshrana@gmail.com','niteshrana754@gmail.com','this is message')
 connection.quit()
